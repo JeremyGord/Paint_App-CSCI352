d49feefd2c697411eacc449090383cf701872551
Jeremy Gordon <jerrgord@ut.utm.edu>
2022-02-03 11:13:18 -0600

    All features implemented and functioning without errors

--- Paint APP/MainWindow.xaml.cs
+++ Paint APP/MainWindow.xaml.cs
@@ -15,9 +15,11 @@ using System.Windows.Shapes;
 
 namespace Paint_APP
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// Jeremy Gordon
+    /// PaintApp
+    /// 2/3/2022
+    /// Assignment for grade: provide functionality to change pen size, color and reset the pen
+    /// back to original settings. Needed the ability to clear the canvas.
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -28,6 +30,46 @@ namespace Paint_APP
         private void Clear_Click(object sender, RoutedEventArgs e)
         {
             Canv.Strokes.Clear();
+           
         }
-    }
+
+        private void ColorGrn_Click(object sender, RoutedEventArgs e)
+        {
+            Canv.DefaultDrawingAttributes.Color = Colors.Green;
+        }
+
+        private void ColorBlu_Click(object sender, RoutedEventArgs e)
+        {
+            Canv.DefaultDrawingAttributes.Color = Colors.Blue;
+        }
+
+        private void ColorRed_Click(object sender, RoutedEventArgs e)
+        {
+            Canv.DefaultDrawingAttributes.Color = Colors.Red;
+        }
+
+        private void ColorPurp_Click(object sender, RoutedEventArgs e)
+        {
+            Canv.DefaultDrawingAttributes.Color = Colors.Purple;
+        }
+
+        private void Thickness_Click(object sender, RoutedEventArgs e)
+        {
+            Canv.DefaultDrawingAttributes.Width = 10;
+            Canv.DefaultDrawingAttributes.Height = 10;
+        }
+
+        private void thinPen_Click(object sender, RoutedEventArgs e)
+        {
+            Canv.DefaultDrawingAttributes.Width = 1;
+            Canv.DefaultDrawingAttributes.Height = 1;
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            Canv.DefaultDrawingAttributes.Color = Colors.Black;
+            Canv.DefaultDrawingAttributes.Width = 2;
+            Canv.DefaultDrawingAttributes.Height = 2;
+        }
+    }  
 }
